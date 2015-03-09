beed35fda7395fa65cc62b4823a7285a8271941d
Adrian Chen <adrian.evangelion@gmail.com>
2015-03-09 16:38:04 +0000

    fixed bots

--- app/lib/game/entities/bots.coffee
+++ app/lib/game/entities/bots.coffee
@@ -1,11 +1,11 @@
-ig.module("game.entities.Character")
+ig.module("game.entities.bots")
   .requires(
     "plusplus.abstractities.character"
   )
   .defines ->
-    ig.EntityBot = ig.global.EntityBot = ig.Character.extend(
+    ig.EntityBots = ig.global.EntityBots = ig.Character.extend(
       animSheet:
-        new ig.AnimationSheet( "media/bot.png", 32, 32)
+        new ig.AnimationSheet( "media/dummy.png", 32, 32)
       animInit:
         "idleX"
       animSettings:
--- app/lib/game/entities/bots.js
+++ app/lib/game/entities/bots.js
@@ -1,6 +1,6 @@
-ig.module("game.entities.Character").requires("plusplus.abstractities.character").defines(function() {
-  return ig.EntityBot = ig.global.EntityBot = ig.Character.extend({
-    animSheet: new ig.AnimationSheet("media/bot.png", 32, 32),
+ig.module("game.entities.bots").requires("plusplus.abstractities.character").defines(function() {
+  return ig.EntityBots = ig.global.EntityBots = ig.Character.extend({
+    animSheet: new ig.AnimationSheet("media/dummy.png", 32, 32),
     animInit: "idleX",
     animSettings: {
       idleX: {
